1eccd4363b07cef653aedfaf48528fe2a7e24bba
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-21 01:17:29 +0530

    Add sign up button when there is no user session

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,7 +6,7 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :validatable
   before_save { self.email = email.downcase }
   validates :name, presence: true, uniqueness: { case_sensitive: false },
-                       length: { minimum: 3, maximum: 25 }
+                       length: { minimum: 6, maximum: 25 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 50 },
