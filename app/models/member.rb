6ff67468b6c280f541d99c1499070289a6b35a44
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-20 19:03:28 +0530

    Added Validation to member and post

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,13 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 50 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true, length: { minimum: 5, maximum: 25 }
 end
